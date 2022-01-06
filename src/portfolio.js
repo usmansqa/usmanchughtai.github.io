7a6cc22b45309564adf3b68d63f68c4696fbbf14
Usman Chughtai <97032412+usmansqa@users.noreply.github.com>
2022-01-06 18:03:04 +0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -129,7 +129,7 @@ const educationInfo = {
     },
     {
       schoolName: "Kips College",
-      logo: require("./assets/images/stanfordLogo.png"),
+      logo: require("./assets/images/kipsLogo.png"),
       subHeader: "Fsc Pre Engineering",
       duration: "September 2013 - September 2015",
       desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
@@ -243,50 +243,40 @@ const achievementSection = {
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
+      title: "Scrum Fundamentals Certified",
       subtitle:
         "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
       image: require("./assets/images/codeInLogo.webp"),
       footerLink: [
         {
           name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
-        },
-        {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+          url: "https://c46e136a583f7e334124-ac22991740ab4ff17e21daf2ed577041.ssl.cf1.rackcdn.com/Certificate/ScrumFundamentalsCertified-UsmanChughtai-837921.pdf"
         }
       ]
     },
     {
-      title: "Google Assistant Action",
+      title: "Selenium WebDriver with Java",
       subtitle:
         "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
       image: require("./assets/images/googleAssistantLogo.webp"),
       footerLink: [
         {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+          name: "Certification",
+          url: "https://testautomationu.applitools.com/certificate/?id=09008b20"
         }
       ]
     },
 
     {
-      title: "PWA Web App Developer",
+      title: "Google Analytics",
       subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
       image: require("./assets/images/pwaLogo.webp"),
-      footerLink: [
-        {name: "Certification", url: ""},
+       footerLink: [
         {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
+          name: "Certification",
+          url: "https://analytics.google.com/analytics/academy/certificate/g9kaNv_CSTqvq_YDRv19xA"
         }
       ]
-    }
   ],
   display: true // Set false to hide this section, defaults to true
 };
